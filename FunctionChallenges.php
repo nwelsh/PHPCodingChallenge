6748be79c168a3268ad13a7f1311efdb4396418b
nwelsh <newelsh@wisc.edu>
2022-08-10 10:57:41 -0500

    array sum

--- FunctionChallenges.php
+++ FunctionChallenges.php
@@ -10,3 +10,16 @@ function giveMeSomething($a) {
 function remainder($x, $y) {
 	return $x % $y;
 }
+
+<!-- Given two unique integer arrays $a and $b, and an integer target value $v, create a function to determine whether there is a pair of numbers that add up to the target value $v, where one number comes from one array $a and the other comes from the second array $b. -->
+function sumOfTwo($a, $b, $v) {
+	// a = arr1 b = arr2 v = target 
+	foreach ($a as $x){
+		foreach ($b as $y){
+			if($x + $y == $v){
+				return true;
+			}
+		}
+	}
+	return false;
+}
